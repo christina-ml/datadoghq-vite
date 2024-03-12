aa36e5582670655195423b7bf2ea476860146060
Christina Loiacono <loiaconochristina@gmail.com>
2024-03-12 12:27:49 -0400

    add nav links and logo

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,16 +1,39 @@
 import "./Navbar.scss";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 const Navbar = () => {
 	return (
 		<div className="Navbar">
             <div className="Navbar__banner">Read the 2023 State of Cloud Security Study!</div>
-			<div className="Navbar__logoContainer">
-				<img
-					src="https://imgix.datadoghq.com/img/about/presskit/logo-v/dd_vertical_purple.png?auto=format&fit=max&w=847&dpr=2"
-					className="Navbar__logoContainer__logo"
-					alt="Datadog logo"
-				/>
-			</div>
+            <div className="Navbar__main">
+                <div className="Navbar__main__left">
+                    <ul>
+                        <li>Product</li>
+                        <li>Customers</li>
+                        <li>Pricing</li>
+                        <li>Solutions</li>
+                        <li>Docs</li>
+                    </ul>
+                </div>
+                <div className="Navbar__main__logoContainer">
+                    <img
+                        src="https://imgix.datadoghq.com/img/about/presskit/logo-v/dd_vertical_purple.png?auto=format&fit=max&w=847&dpr=2"
+                        className="Navbar__main__logoContainer__logo"
+                        alt="Datadog logo"
+                    />
+                </div>
+                <div className="Navbar__main__right">
+                    <ul>
+                        <li>About</li>
+                        <li>Blog</li>
+                        <li>Login</li>
+                        <li><FaMagnifyingGlass /></li>
+                        <li className="Navbar__main__right__getStarted">
+                            <button>Get Started Free</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
 		</div>
 	);
 };
